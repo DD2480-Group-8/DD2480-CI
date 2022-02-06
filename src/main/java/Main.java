59bf0e37755df839b99a54444d36cc5ce0b61a49
Emil Myresten <emil.myresten@gmail.com>
2022-02-06 18:47:30 +0100

    build: adds initial maven project structure and junit 4.12 (#4)
    Also updates .gitignore to not include .idea or target files.

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static boolean isEqualsToOne(int x) {
+        return x == 1;
+    }
+}
