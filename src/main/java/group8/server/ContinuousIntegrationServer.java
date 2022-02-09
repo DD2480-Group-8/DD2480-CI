cab3cea4406ac68e4ab8108cab49167d36b733dc
Emil Myresten <emil.myresten@gmail.com>
2022-02-09 14:06:03 +0100

    feat: adds mvn verify functionality to cloned repo (#8)

--- src/main/java/group8/server/ContinuousIntegrationServer.java
+++ src/main/java/group8/server/ContinuousIntegrationServer.java
@@ -4,17 +4,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
+import group8.CurrentDate;
+import group8.MavenTester;
+import group8.TestLogger;
 import group8.Utilities;
+import group8.git.GitRepoFetcher;
 import org.apache.maven.shared.invoker.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
@@ -30,14 +33,14 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 */
 public class ContinuousIntegrationServer extends AbstractHandler {
 
-    private Invoker invoker;
+//    private Invoker invoker;
 
-    public void invokerInit(File repoLocation) {
-        Invoker newInvoker = new DefaultInvoker();
-        newInvoker.setLocalRepositoryDirectory(repoLocation);
-
-        this.invoker = newInvoker;
-    }
+//    public void invokerInit(File repoLocation) {
+//        Invoker newInvoker = new DefaultInvoker();
+//        newInvoker.setLocalRepositoryDirectory(repoLocation);
+//
+//        this.invoker = newInvoker;
+//    }
 
     public void handle(String target,
                        Request baseRequest,
@@ -52,39 +55,30 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         String requestBody = Utilities.getRequestBodyAsString(request);
         JsonObject requestJson = Utilities.deserializeRequest(requestBody);
 
-        System.out.println(requestJson);
-        System.out.println(requestJson.get("ref"));
-        System.out.println(target);
+//        System.out.println(requestJson);
+//        System.out.println(requestJson.get("ref"));
+        System.out.println(String.format("request received on %s", target));
+
+        String currentDate = CurrentDate.getCurrentDate();
 
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
-        System.out.println("Cloned repo");
-        File dirLoc = new File("/test/");
-        try {
-            Git git = Git.cloneRepository()
-                            .setURI("https://github.com/DD2480-Group-8/DD2480-DECIDE.git")
-                            .setDirectory(dirLoc)
-                            .call();
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Cloning repo...");
+        File destinationDirectory = new File(String.format("./builds/DD2480-CI-%s", currentDate));
+        GitRepoFetcher repo = new GitRepoFetcher("https://github.com/DD2480-Group-8/DD2480-CI.git");
+        repo.fetchToDestination(destinationDirectory);
+        System.out.println("Repo cloned.");
 
-        List<String> goals = Arrays.asList("clean", "test");
-        InvocationRequest req = new DefaultInvocationRequest();
-        req.setBaseDirectory(dirLoc);
-        req.setGoals(goals);
-        invokerInit(dirLoc);
 
-        InvocationResult res = null;
-        try {
-            res = invoker.execute(req);
-        } catch (MavenInvocationException e) {
-            System.out.println("Maven test failed, error code: " + res.getExitCode());
-        }
+        Process process = MavenTester.mvnVerifyInDirectory(destinationDirectory);
+        TestLogger log = new TestLogger();
+        log.logProcess(process);
+        log.writeLogToDestination(destinationDirectory);
 
-        response.getWriter().println(res.getExitCode());
+        System.out.println(currentDate);
+
         response.getWriter().println("CI job done");
     }
  
