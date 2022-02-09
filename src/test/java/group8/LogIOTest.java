152557ea60d46c207476108e17e564dad5071946
Viktor Åryd <varyd@kth.se>
2022-02-09 15:00:08 +0100

    feat: added class for reading logs and identifying errors/fails (#11)

---
+++ src/test/java/group8/LogIOTest.java
@@ -0,0 +1,16 @@
+package group8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LogIOTest {
+    
+    
+    /**
+     * TODO: Add some way to test function properly.  
+     */
+    @Test
+    public void getBuildResultTest(){
+        Assert.assertTrue(true);
+    }
+}
